975c24760b4ec1a108706a30d677168eb4e56705
Grégoire Cartier <infernoxe42@hotmail.fr>
2017-10-03 15:54:35 +0200

    Signed-off-by: Grégoire Cartier <infernoxe42@hotmail.fr>

---
+++ model.js
@@ -0,0 +1,31 @@
+class Teacher{
+    constructor(id, name){
+        this.id = id;
+        this.name = name;
+    }
+}
+
+class Student{
+    constructor(id, name){
+        this.id = id;
+        this.name = name;
+    }
+}
+
+class Classroom{
+    constructor(id){
+        this.id = id;
+    }
+}
+
+class Course{
+    constructor(id,subject,time,date,teacher,classroom){
+        this.id = id;
+        this.subject = subject;
+        this.time = time;
+        this.date = date;
+        this.students = [];
+        this.teacher = teacher;
+        this.classroom = classroom
+    }
+}
