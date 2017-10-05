9291c063c0b201b0af8f903103f36f56c5a64564
Grégoire Cartier <infernoxe42@hotmail.fr>
2017-10-05 13:34:01 +0200

    Hard coded model

--- model.js
+++ model.js
@@ -3,6 +3,10 @@ class Teacher{
         this.id = id;
         this.name = name;
     }
+        getInfo(){
+            return "Teacher:" +  this.id + ' ' + this.name;
+        }
+    
 }
 
 class Student{
@@ -10,11 +14,18 @@ class Student{
         this.id = id;
         this.name = name;
     }
+
+    getInfo(){
+        return "Student:" +  this.id + ' ' + this.name;
+    }
 }
 
 class Classroom{
     constructor(id){
         this.id = id;
+    }
+    getInfo(){
+        return "Classroom:" +  this.id;
     }
 }
 
@@ -28,5 +39,34 @@ class Course{
         this.teacher = teacher;
         this.classroom = classroom
     }
+
+    addStudents(obj){
+        this.students.push(obj)
+    }
+
+    toStringCourse(){
+        console.log(this.id + " " + this.subject + " " + this.time + " " + this.date + " " + this.teacher.getInfo() + " " + this.classroom.getInfo());
+        for(var i =  0; i < this.students.length; i++){
+            console.log(this.students[i].getInfo());
+        }
+
+    }
 }
+
+
+stu1 = new Student(1,"Bob");
+stu2 = new Student(2,"Joe");
+stu3 = new Student(3,"Doe");
+
+tea1 = new Teacher(1,"Henry");
+
+class1 = new Classroom(25);
+
+cou1 = new Course(1,"JavaScript","15:30","12/01",tea1,class1)
+cou1.addStudents(stu1);
+cou1.addStudents(stu2);
+cou1.addStudents(stu3);
+
+cou1.toStringCourse();
+
 
